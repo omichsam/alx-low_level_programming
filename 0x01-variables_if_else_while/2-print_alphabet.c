bb4b355b3dd28d0df65b2732577bc16d29b124bd
omichsam <omichsam@gmail.com>
2023-02-20 21:53:19 +0300

    works

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase\n
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int ch;
