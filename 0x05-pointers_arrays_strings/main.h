ee958bbde0ae78900194aba564deccfdb0d71548
omichsam <omichsam@gmail.com>
2023-02-28 21:11:09 +0300

    trial l

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-#include <stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,7 +11,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-
 char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
