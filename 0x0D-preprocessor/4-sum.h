ee00390a59956378e9f7744607aa435968f39aec
omichsam <omichsam@gmail.com>
2023-03-21 12:57:30 +0300

    Creating a header file that defines a macro named PI corrected.

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,5 +1,13 @@
-#ifndef SUM
+#ifndef SUM_H
+#define SUM_
+
+/*
+ * File: 4-sum.h
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
+ */
 
 #define SUM(x, y) ((x) + (y))
 
 #endif
+
