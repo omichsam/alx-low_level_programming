fa6048010135c30f65500a229d17fb727c6cbe72
omichsam <omichsam@gmail.com>
2023-04-11 12:14:32 +0300

    correcte 0-binary_to_int file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,25 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-/**
- * binary_to_uint - change a binary number to an unsigned int
- * @b: char string
- * Return: the converted decimal number or 0 if there is an unconvertible char
- */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *binary_string)
 {
-    unsigned int total = 0; // initialize total to 0
-    unsigned int power = 1; // initialize power to 1
-    int length;
+    unsigned int decimal_value = 0;
+    int index;
 
-    if (b == NULL) // check if string is null
-        return 0;
+    /* if string is empty return 0 */
+    if (!binary_string)
+        return (0);
 
-    for (length = 0; b[length]; length++) // calculate length of the string
+    /* return if the value is different to 0 or 1*/
+    for (index = 0; binary_string[index] != '\0'; index++)
+        if (binary_string[index]  != '0' && binary_string[index] != '1')
+            return (0);
+
+    /* converter array the number binary to int */
+    for (index = 0; binary_string[index] != '\0'; index++)
     {
-        if (b[length] != '0' && b[length] != '1') // check if each character is valid
-            return 0;
+        decimal_value *= 2;
+        if (binary_string[index] == '1')
+            decimal_value += 1;
     }
-
-    for (length--; length >= 0; length--, power *= 2) // iterate through the string in reverse order
-    {
-        if (b[length] == '1') // if the current character is 1, add the corresponding power of 2 to the total
-            total += power;
-    }
-
-    return total; // return the converted decimal number
+    return (decimal_value);
 }
