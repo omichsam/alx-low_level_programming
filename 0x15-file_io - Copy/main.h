fb9ed052ccd20c947385e9ed5afdf97f6ab68b21
omichsam <omichsam@gmail.com>
2023-04-13 12:22:25 +0300

    0-read_textfile.c file

--- 0x15-file_io - Copy/main.h
+++ 0x15-file_io - Copy/main.h
@@ -1,9 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H 
+#define MAIN_H 
 
 /*
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
+ * Desc: Header file containing prototypes for functions
+ *       written and used in the 0x14-file_io directory.
  */
 
 #include <sys/types.h>
@@ -11,9 +11,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int _putchar(char c);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
 #endif /* MAIN_H */
