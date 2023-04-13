203626b2c970e4ef2d4b8294514114dc74dc2e4b
omichsam <omichsam@gmail.com>
2023-04-13 12:02:29 +0300

    updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,20 @@
-#IFNDEF MAIN_H
-#DEFINE MAIN_H
+#ifndef MAIN_H 
+#define MAIN_H 
 
-size_t read_textfile(const char *filename, size_t letters);
+/*
+ * Desc: Header file containing prototypes for functions
+ *       written and used in the 0x14-file_io directory.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
-#ENDIF
+#endif /* MAIN_H */
