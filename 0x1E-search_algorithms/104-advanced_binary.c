c84d08d56b18a2bbb1ae780070864113f4db5bfd
omichsam <omichsam@gmail.com>
2023-12-26 11:44:17 +0300

    advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,33 +1,64 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - Search for value in sorted array
- * @array: Pointer to first element of array
- * @size: Elements in array
- * @value: Value being searched for
+ * rec_search - searches for a value in an array of
+ * integers using the Binary search algorithm
  *
- * Return: Index where value found
- * -1 if value not found or NULL array
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int rec_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (rec_search(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (rec_search(array, half + 1, value));
+
+	half++;
+	return (rec_search(array + half, size - half, value) + half);
+}
+
+/**
+ * advanced_binary - calls to rec_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t i;
+	int index;
 
-	printf("Searching in array: ");
-	for (i = 0; i < size; ++i)
-	{
-		printf("%d", array[i]);
-		if (i < size - 1)
-			printf(", ");
-	}
-	printf("\n");
-	if (size == 1)
-		return (*array == value ? *array : -1);
-	if (array[size / 2] < value)
-		return (advanced_binary(array + (size / 2) + 1, size / 2, value));
-	if (array[size / 2] == value)
-		return (array[size / 2]);
-	return (advanced_binary(array, size / 2, value));
+	index = rec_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
 }
-/* size is n */
-/* value is x */
