f85814f378b3cf9f9402c99134405c07ec5ca643
omichsam <omichsam@gmail.com>
2023-12-26 11:43:24 +0300

    linear updated

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,23 +1,24 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for value in array of integers using Linear search
- * @array: Pointer to first element of array to search
- * @size: Number of elements in array
- * @value: Value to search
- *
- * Return: First index where value is found
- * -1 if value not in array or array is NULL
- *  Print value every time comparing value to that being searched for
+ * linear_search - searches for a value in an array of integers using the
+ * Linear search algorithm
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search for
+ * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	int i;
 
-	for (i = 0; array && i < size; ++i)
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
 			return (i);
 	}
 	return (-1);
